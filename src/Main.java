fa6b61442a06ca588786088ff2623ccc2678afca
Java <Java@10.48.100.29>
2019-12-03 22:04:43 +0000

    Apaguei tudo!!!!

--- src/Main.java
+++ src/Main.java
@@ -4,29 +4,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Teste");
 		
-		System.out.println("Olá Castro...");
-		
-		System.out.println("A bombar código!!!");
-		
-		System.out.println("Espetaculo!");
-		
-		int x;
-		int y;
-		int z;
-		
-		System.out.println("Coiso");
-		
-		System.out.println("Teste A");
-		
-		
-		
-		System.out.println("Cenas...");
-
-		System.out.println("Testeb");
-		
-		System.out.println("ola");
-		
-		System.out.println("asdasdasd");
 	}
 
 }
