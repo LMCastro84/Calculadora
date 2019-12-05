692da34ae354a1dbc9a35b2f5b7b84559f53c304
Java <Java@10.48.100.29>
2019-12-05 21:47:00 +0000

    Alterei a Switch

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("|==============Calculadora==============|");
+		System.out.println("|==============CALCULADORA==============|");
 		
 		double x;
 		double y;
@@ -14,13 +14,14 @@ public class Main {
 		Scanner in = new Scanner(System.in);
 
 		System.out.println();
-		System.out.println("|------------Inserir calculo------------|");
-		System.out.println("|1 -           Multiplicar;             |");
-		System.out.println("|2 -             Dividir;               |");
-		System.out.println("|3 -              Somar;                |");
-		System.out.println("|4 -            Subtrair;               |");
+		System.out.println("|--------------Operaçãoes---------------|");
+		System.out.println("|1 -           Multiplicar              |");
+		System.out.println("|2 -             Dividir                |");
+		System.out.println("|3 -              Somar                 |");
+		System.out.println("|4 -            Subtrair                |");
 		System.out.println("|_______________________________________|");
 		
+		System.out.println("Escolha uma operação aritmética:_");
 		opcao = in.nextInt();
 		
 		switch (opcao){
@@ -33,25 +34,31 @@ public class Main {
 				System.out.println(x + " x " + y + " = " + z);
 				break;
 			case 2:
-				System.out.print("|-------Introduzir valor numÃ©rico:------|");
+				System.out.print("Introduzir um valor:_");
+				x = in.nextInt();
+				System.out.print("Introduzir outro valor:_");
 				y = in.nextInt();
 				z = x / y;
 				System.out.println(x + " : " + y + " = " + z);
 				break;
 			case 3:
-				System.out.print("|-------Introduzir valor numÃ©rico:------|");
+				System.out.print("Introduzir um valor:_");
+				x = in.nextInt();
+				System.out.print("Introduzir outro valor:_");
 				y = in.nextInt();
 				z = x + y;
 				System.out.println(x + " + " + y + " = " + z);
 				break;
 			case 4:
-				System.out.print("|-------Introduzir valor numÃ©rico:------|");
+				System.out.print("Introduzir um valor:_");
+				x = in.nextInt();
+				System.out.print("Introduzir outro valor:_");
 				y = in.nextInt();
 				z = x - y;
 				System.out.println(x + " - " + y + " = " + z);
 				break;
 			default:
-				System.out.println("|-------Introduziu um valor errado. Tente novamente.------|");
+				System.out.println("Seleccionou uma opção incorrecta. Tente novamente.");
 				// reiniciar a switch
 		}
 	}
