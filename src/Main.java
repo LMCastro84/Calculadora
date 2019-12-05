a6154f3262ee6b71737f8a9f2a65b6b8402ba904
LMCastro84 <58310131+LMCastro84@users.noreply.github.com>
2019-12-05 14:05:16 +0000

    Add switch (case 1+case2) multiplicar+dividir

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,49 @@
+import java.util.Scanner;
+
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Teste");
 		
-		System.out.println("Isto nao � aldrabice!!!");
-		
+		System.out.println("Calculadora");
+		System.out.print("Introduzir valor numérico para calcular:");
+		Scanner in = new Scanner(System.in);
+		int x, y;
+		double z;
+		x = in.nextInt();
+		int opcao = 0;
+		System.out.println("inserir calculo");
+		System.out.println("1 - Multiplicar;");
+		System.out.println("2 - Dividir;");
+		System.out.println("3 - Somar;");
+		System.out.println("4 - Subtrair;");
+		opcao = in.nextInt();
+		switch (opcao){
+			case 1:
+				System.out.print("Introduzir valor numérico:");
+				y = in.nextInt();
+				z = x * y;
+				System.out.println(x + " x " + y + " = " + z);
+				break;
+			case 2:
+				System.out.print("Introduzir valor numérico:");
+				y = in.nextInt();
+				z = x / y;
+				System.out.println(x + " : " + y + " = " + z);
+				break;
+//			case 3:
+//				System.out.print("Introduzir valor numérico:");
+//				y = in.nextInt();
+//				z = x + y;
+//				System.out.println(x + " + " + y + " = " + z);
+//				break;
+//			case 4:
+//				System.out.print("Introduzir valor numérico:");
+//				y = in.nextInt();
+//				z = x - y;
+//				System.out.println(x + " - " + y + " = " + z);
+//				break;
+		}
 	}
-
 }
+				
